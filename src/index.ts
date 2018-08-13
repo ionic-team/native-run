a14422b98a01e829d65628dead6598c577b76cbe
Tim Lancina <tim@ionic.io>
2018-08-13 16:40:37 -0500

    really simple CLI

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log('hello world');
+export async function run() {
+  const args = process.argv.slice(2);
+  if (args.length === 0 || args.includes('--help') || args.includes('help') || args.includes('-h')) {
+    //TODO: show help
+    return;
+  }
+
+  const [platform] = args;
+  if (platform !== 'android' && platform !== 'ios') {
+    console.log(`Unsupported platform: ${platform}`);
+  }
+
+  if (process.argv.includes('--list')) {
+    // TODO: list devices/emulators
+    const list = await import('./list');
+    return await list.run();
+  }
+}
