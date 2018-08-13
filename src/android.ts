f6a7dac44896b483511df9792da1dba9de9a6e03
Daniel Imhoff <dwieeb@gmail.com>
2018-08-13 18:50:09 -0500

    integrate linter

--- src/android.ts
+++ src/android.ts
@@ -1,6 +1,7 @@
 import { RunOptions } from './definitions';
 
 export async function getDevices() {
+  // TODO: get android devices
 }
 
 export async function run(device: string, options: RunOptions) {
--- src/list.ts
+++ src/list.ts
@@ -1,10 +1,10 @@
+// import { getDevices as getAndroidDevices } from './android';
 import { RunOptions } from './definitions';
-import { getDevices as getAndroidDevices } from './android';
 
 export async function run(platform: string | undefined, options: RunOptions) {
   // TODO: do something with platform arg
 
-  const androidDevices = await getAndroidDevices();
+  // const androidDevices = await getAndroidDevices();
 
-  console.log(androidDevices);
+  // console.log(androidDevices);
 }
