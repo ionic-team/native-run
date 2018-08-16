829420c85c10c76ad7f0722ea5794d235d1f463f
Tim Lancina <tim@ionic.io>
2018-08-16 18:22:05 -0500

    get iOS device info

--- src/ios/run.ts
+++ src/ios/run.ts
@@ -1,4 +1,49 @@
+import { spawnSync } from 'child_process'; // TODO: need cross-spawn for windows?
+
+export type UDID = string;
+export interface IOSDevice {
+  readonly deviceClass: string;
+  readonly deviceName: string;
+  readonly productType: string; // TODO: map to actual model (ie. iPhone8,4 -> iPhone SE)
+  readonly productVersion: string;
+}
+
+export async function getConnectedDevicesUDIDs(): Promise<UDID[]> {
+  const iDeviceId = spawnSync('idevice_id', ['--list'], { encoding: 'utf8' });
+  // split results on \n
+  return iDeviceId.stdout.match(/.+/g) || [];
+}
+
+export async function getConnectedDevicesInfo(udids: UDID[]): Promise<IOSDevice[]> {
+  return Promise.all(
+    udids.map(udid => getConnectedDeviceInfo(udid))
+  );
+}
+
+export async function getConnectedDeviceInfo(udid: UDID): Promise<IOSDevice> {
+  const iDeviceInfo = spawnSync('ideviceinfo', ['--simple', '--udid', udid], { encoding: 'utf8' });
+  return parseDeviceInfo(iDeviceInfo.stdout);
+}
+
+function parseDeviceInfo(deviceInfo: string): IOSDevice {
+  return {
+    deviceClass: matchDeviceProperty(deviceInfo, 'DeviceClass'),
+    deviceName: matchDeviceProperty(deviceInfo, 'DeviceName'),
+    productType: matchDeviceProperty(deviceInfo, 'ProductType'),
+    productVersion: matchDeviceProperty(deviceInfo, 'ProductVersion'),
+  };
+}
+
+function matchDeviceProperty(deviceInfo: string, prop: string) {
+  const result = deviceInfo.match(new RegExp(`${prop}:\\s+(.+)`));
+  if (!result || result.length !== 2) {
+    return '';
+  }
+  return result[1];
+}
 
 export async function run(args: string[]) {
-  process.stdout.write('run');
+  const udids = await getConnectedDevicesUDIDs();
+  const info: any = await getConnectedDevicesInfo(udids);
+  console.log(info);
 }
--- src/ios/utils.ts
+++
@@ -1,9 +0,0 @@
-/* tslint:disable */
-export type UDID = string;
-
-export async function getConnectedDeviceUDIDs(): Promise<UDID[]> {
-  const child_process = await import('child_process');
-  const iDeviceId = child_process.spawnSync('idevice_id', ['-l'], { encoding: 'utf8' });
-  // split results on \n
-  return iDeviceId.stdout.match(/.+/g) || [];
-}
