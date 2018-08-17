58401bd8834e09a9837ce91b04d3a980099323d9
Daniel Imhoff <dwieeb@gmail.com>
2018-08-17 14:52:19 -0500

    catch all the errors

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
+import * as Debug from 'debug';
+
+const debug = Debug('native-run');
+
 export async function run() {
   const args = process.argv.slice(2);
 
@@ -9,19 +13,25 @@ export async function run() {
 
   const [ platform, ...platformArgs ] = args;
 
-  if (platform === 'android') {
-    const lib = await import('./android');
-    await lib.run(platformArgs);
-  } else if (platform === 'ios') {
-    const lib = await import('./ios');
-    await lib.run(platformArgs);
-  } else {
-    if (!platform || platform === 'help' || args.includes('--help') || args.includes('-h') || platform.startsWith('-')) {
-      const help = await import('./help');
-      return help.run();
-    }
+  try {
+    if (platform === 'android') {
+      const lib = await import('./android');
+      await lib.run(platformArgs);
+    } else if (platform === 'ios') {
+      const lib = await import('./ios');
+      await lib.run(platformArgs);
+    } else {
+      if (!platform || platform === 'help' || args.includes('--help') || args.includes('-h') || platform.startsWith('-')) {
+        const help = await import('./help');
+        return help.run();
+      }
 
-    process.stderr.write(`Unsupported platform: "${platform}"\n`);
+      process.stderr.write(`Unsupported platform: "${platform}"\n`);
+      process.exitCode = 1;
+    }
+  } catch (e) {
+    debug('Caught fatal error: %O', e);
+    process.stderr.write(String(e.stack ? e.stack : e));
     process.exitCode = 1;
   }
 }
