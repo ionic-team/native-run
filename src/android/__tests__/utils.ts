5012462d950b329ca0af6be104d5abdade0ac39a
Daniel Imhoff <dwieeb@gmail.com>
2018-08-17 17:41:27 -0500

    add tests for more AVDs

--- src/android/__tests__/utils.ts
+++ src/android/__tests__/utils.ts
@@ -44,6 +44,42 @@ describe('android/utils', () => {
       expect(avd).toEqual(expected);
     });
 
+    it('should properly parse Pixel_2_XL_API_28', async () => {
+      const ini = await utils.readINI(path.resolve(__dirname, './fixtures/avd/Pixel_2_XL_API_28.ini'));
+      ini.path = path.resolve(__dirname, './fixtures/avd/Pixel_2_XL_API_28.avd'); // patch path
+
+      const expected = {
+        id: 'Pixel_2_XL_API_28',
+        path: ini.path,
+        name: 'Pixel 2 XL API 28',
+        target: 28,
+        screenDPI: 560,
+        screenWidth: 1440,
+        screenHeight: 2880,
+      };
+
+      const avd = await androidUtils.getAVDFromINI(ini);
+      expect(avd).toEqual(expected);
+    });
+
+    it('should properly parse avdmanager_1', async () => {
+      const ini = await utils.readINI(path.resolve(__dirname, './fixtures/avd/avdmanager_1.ini'));
+      ini.path = path.resolve(__dirname, './fixtures/avd/avdmanager_1.avd'); // patch path
+
+      const expected = {
+        id: 'avdmanager_1',
+        path: ini.path,
+        name: 'avdmanager 1',
+        target: 28,
+        screenDPI: null,
+        screenWidth: null,
+        screenHeight: null,
+      };
+
+      const avd = await androidUtils.getAVDFromINI(ini);
+      expect(avd).toEqual(expected);
+    });
+
   });
 
 });
