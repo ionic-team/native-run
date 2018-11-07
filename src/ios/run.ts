5f426a875481510cf87248cb066b2a45d139286f
Tim Lancina <tim@ionic.io>
2018-11-07 09:35:29 -0800

    ios run code cleanup

--- src/ios/run.ts
+++ src/ios/run.ts
@@ -1,28 +1,34 @@
-import { readDir, statSafe } from '@ionic/utils-fs';
+import { mkdirp, readDir } from '@ionic/utils-fs';
 import * as Debug from 'debug';
-import * as fs from 'fs';
-import { AFCError, AFC_STATUS, ClientManager } from 'node-ioslib';
+import { createWriteStream, mkdtempSync, readFileSync } from 'fs';
+import { AFCError, AFC_STATUS, ClientManager, IPLookupResult } from 'node-ioslib';
 import * as path from 'path';
-import { Entry } from 'yauzl';
+import { Readable } from 'stream';
+import { promisify } from 'util';
+import { Entry, Options, ZipFile } from 'yauzl';
 
 import { RunException } from '../errors';
 import { getOptionValue } from '../utils/cli';
 import { execFile } from '../utils/process';
 
 const debug = Debug('native-run:ios:run');
+const wait = promisify(setTimeout);
 
 export async function run(args: string[]) {
-  const { app, udid } = await validateArgs(args);
+  let appPath = getOptionValue(args, '--app');
+  if (!appPath) {
+    throw new RunException('--app argument is required.');
+  }
+  const udid = getOptionValue(args, '--target');
   const clientManager = await ClientManager.create(udid);
-  const isIPA = app.endsWith('.ipa');
-  let appPath = app;
+  const isIPA = appPath.endsWith('.ipa');
 
   try {
     await mountDeveloperDiskImage(clientManager);
 
     if (isIPA) {
       const { tmpdir } = await import('os');
-      const tempDir = fs.mkdtempSync(`${tmpdir()}${path.sep}`);
+      const tempDir = mkdtempSync(`${tmpdir()}${path.sep}`);
       debug(`Unzipping .ipa to ${tempDir}`);
       const appDir = await unzipApp(appPath, tempDir);
       appPath = path.join(tempDir, appDir);
@@ -38,13 +44,13 @@ export async function run(args: string[]) {
     await installer.installApp(destPackagePath, bundleId);
 
     const { [bundleId]: appInfo } = await installer.lookupApp([bundleId]);
-    await wait(200); // launch fails with EBusy or ENotFound if you try to launch immediately after install
+    // launch fails with EBusy or ENotFound if you try to launch immediately after install
+    await wait(200);
     await launchApp(clientManager, appInfo);
+
   } finally {
     if (isIPA) {
-      try {
-        (await import('rimraf')).sync(appPath);
-      } catch (err) {} // tslint:disable-line
+      try { (await import('rimraf')).sync(appPath); } catch { } // tslint:disable-line
     }
     clientManager.end();
   }
@@ -59,10 +65,7 @@ async function mountDeveloperDiskImage(clientManager: ClientManager) {
     const version = await (await clientManager.getLockdowndClient()).getValue('ProductVersion');
     const xCodePath = await getXCodePath();
     const developerDiskImagePath = await getDeveloperDiskImagePath(version, xCodePath);
-    const developerDiskImageSig = fs.readFileSync(`${developerDiskImagePath}.signature`);
-    if (!statSafe(developerDiskImagePath)) {
-      throw new Error(`No Developer Disk Image found for SDK ${version} at\n${developerDiskImagePath}.`);
-    }
+    const developerDiskImageSig = readFileSync(`${developerDiskImagePath}.signature`);
     await imageMounter.uploadImage(developerDiskImagePath, developerDiskImageSig);
     await imageMounter.mountImage(developerDiskImagePath, developerDiskImageSig);
   }
@@ -82,7 +85,7 @@ async function uploadApp(clientManager: ClientManager, srcPath: string, destinat
   await afcClient.uploadDirectory(srcPath, destinationPath);
 }
 
-async function launchApp(clientManager: ClientManager, appInfo: any) {
+async function launchApp(clientManager: ClientManager, appInfo: IPLookupResult[string]) {
   let tries = 0;
   while (tries < 3) {
     const debugServerClient = await clientManager.getDebugserverClient();
@@ -105,44 +108,29 @@ async function launchApp(clientManager: ClientManager, appInfo: any) {
   throw new RunException('Unable to launch app, number of tries exceeded');
 }
 
-async function validateArgs(args: string[]) {
-  const app = getOptionValue(args, '--app');
-  if (!app) {
-    throw new RunException('--app argument is required.');
-  }
-  const udid = getOptionValue(args, '--target');
-  return { app, udid };
-}
-
 async function getXCodePath() {
   try {
     const { stdout } = await execFile('xcode-select', ['-p'], { encoding: 'utf8' });
-    if (!stdout) {
-      throw new Error('Unable to get Xcode location. Is Xcode installed?');
+    if (stdout) {
+      return stdout.trim();
     }
-    return stdout.trim();
-  } catch (err) {
-    throw new Error('Unable to get Xcode location. Is Xcode installed?');
-  }
+  } catch { } // tslint:disable-line
+  throw new Error('Unable to get Xcode location. Is Xcode installed?');
 }
 
 async function getDeveloperDiskImagePath(version: string, xCodePath: string) {
-  try {
-    const versionDirs = await readDir(`${xCodePath}/Platforms/iPhoneOS.platform/DeviceSupport/`);
-    const versionPrefix = version.match(/\d+\.\d+/);
-    if (versionPrefix === null) {
-      throw new Error(`Invalid iOS version: ${version}`);
-    }
-    // Can look like "11.2 (15C107)"
-    for (const dir of versionDirs) {
-      if (dir.indexOf(versionPrefix[0]) !== -1) {
-        return `${xCodePath}/Platforms/iPhoneOS.platform/DeviceSupport/${dir}/DeveloperDiskImage.dmg`;
-      }
-    }
-    throw new Error(`Unable to find Developer Disk Image path for SDK ${version}. Do you have the right version of Xcode?`);
-  } catch {
-    throw new Error(`Unable to find Developer Disk Image path for SDK ${version}.`);
+  const versionDirs = await readDir(`${xCodePath}/Platforms/iPhoneOS.platform/DeviceSupport/`);
+  const versionPrefix = version.match(/\d+\.\d+/);
+  if (versionPrefix === null) {
+    throw new Error(`Invalid iOS version: ${version}`);
   }
+  // Can look like "11.2 (15C107)"
+  for (const dir of versionDirs) {
+    if (dir.includes(versionPrefix[0])) {
+      return `${xCodePath}/Platforms/iPhoneOS.platform/DeviceSupport/${dir}/DeveloperDiskImage.dmg`;
+    }
+  }
+  throw new Error(`Unable to find Developer Disk Image path for SDK ${version}. Do you have the right version of Xcode?`);
 }
 
 // TODO: cross platform? Use plist/bplist
@@ -152,67 +140,44 @@ async function getBundleId(packagePath: string) {
     const { stdout } = await execFile('/usr/libexec/PlistBuddy',
                                       ['-c', 'Print :CFBundleIdentifier', plistPath],
                                       { encoding: 'utf8' });
-    if (!stdout) {
-      throw new Error('Unable to get app bundle identifier');
+    if (stdout) {
+      return stdout.trim();
     }
-    return stdout.trim();
-  } catch (err) {
-    throw new Error('Unable to get app bundle identifier');
-  }
+  } catch { } // tslint:disable-line
+  throw new Error('Unable to get app bundle identifier');
 }
 
-async function wait(milliseconds: number) {
-  return new Promise(r => setTimeout(r, milliseconds));
-}
+// Override so promisify typing correctly infers params
+type YauzlOpen = (path: string, options: Options, callback?: (err: Error, zipfile: ZipFile) => void) => void;
+type YauzlOpenReadStream = (entry: Entry, callback?: (err: Error, stream: Readable) => void) => void;
 
 async function unzipApp(srcPath: string, destPath: string) {
-  const [yauzl, mkdirp] = await Promise.all([
-    await import('yauzl'),
-    await import ('mkdirp'),
-  ]);
+  const yauzl = await import('yauzl');
+  const open = promisify(yauzl.open.bind(yauzl) as YauzlOpen);
   let appDir = '';
-  return new Promise<string>((resolve, reject) => {
-    yauzl.open(srcPath, { lazyEntries: true }, (err, zipfile) => {
-      if (err) { reject(err); }
-      if (zipfile) {
-        zipfile.on('error', err => reject(err));
-        zipfile.on('end', () => {
-          if (appDir) {
-            resolve(appDir);
-          } else {
-            reject('Unable to determine .app directory from .ipa');
-          }
-        });
+  return new Promise<string>(async (resolve, reject) => {
+    const zipfile = await open(srcPath, { lazyEntries: true });
+    const openReadStream = promisify(zipfile.openReadStream.bind(zipfile) as YauzlOpenReadStream);
+
+    zipfile.once('error', reject);
+    zipfile.once('end', () => { appDir ? resolve(appDir) : reject('Unable to determine .app directory from .ipa'); });
+
+    zipfile.readEntry();
+    zipfile.on('entry', async (entry: Entry) => {
+      debug(`Unzip: ${entry.fileName}`);
+      const dest = path.join(destPath, entry.fileName);
+      if (entry.fileName.endsWith('/')) {
+        await mkdirp(dest);
+        if (entry.fileName.endsWith('.app/')) {
+          appDir = entry.fileName;
+        }
         zipfile.readEntry();
-        zipfile.on('entry', (entry: Entry) => {
-          debug(`Unzip: ${entry.fileName}`);
-          const dest = path.join(destPath, entry.fileName);
-          if (/\/$/.test(entry.fileName)) {
-            mkdirp(dest, err => {
-              if (err) {
-                reject(err);
-              } else {
-                if (entry.fileName.endsWith('.app/')) {
-                  appDir = entry.fileName;
-                }
-                zipfile.readEntry();
-              }
-            });
-          } else {
-            mkdirp(path.dirname(dest), err => {
-              if (err) { reject(err); }
-              // file entry
-              zipfile.openReadStream(entry, (err, readStream) => {
-                if (err) { reject(err); }
-                if (readStream) {
-                  readStream.on('end', () => { zipfile.readEntry(); });
-                  const writeStream = fs.createWriteStream(path.join(destPath, entry.fileName));
-                  readStream.pipe(writeStream);
-                }
-              });
-            });
-          }
-        });
+      } else {
+        await mkdirp(path.dirname(dest));
+        const readStream = await openReadStream(entry);
+        readStream.on('end', () => { zipfile.readEntry(); });
+        const writeStream = createWriteStream(path.join(destPath, entry.fileName));
+        readStream.pipe(writeStream);
       }
     });
   });
