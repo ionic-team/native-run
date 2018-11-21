bc4cca0c8ec25b9c3422980b19040aa224810e2f
Tim Lancina <tim@ionic.io>
2018-11-21 11:59:02 -0800

    make help for each platform be similar

--- src/android/help.ts
+++ src/android/help.ts
@@ -1,7 +1,7 @@
 const help = `
   Usage: native-run android [options]
 
-    Run an APK on a device or emulator target
+    Run an .apk on a device or emulator target
 
     Targets are selected as follows:
       1) --target using device/emulator serial number or AVD ID
@@ -17,10 +17,14 @@ const help = `
 
     --list ............... Print available targets, then quit
     --sdk-info ........... Print SDK information, then quit
+    --json ............... Output JSON
 
-    --apk <path> ......... Deploy specified APK file
+    --app <path> ......... Deploy specified .apk file
     --device ............. Use a device if available
+                           With --list prints connected devices
     --emulator ........... Prefer an emulator
+                           With --list prints available emulators
+    --simulator .......... Alias for --emulator
     --target <id> ........ Use a specific target
     --connect ............ Tie process to app process
 `;
--- src/ios/help.ts
+++ src/ios/help.ts
@@ -1,10 +1,33 @@
 const help = `
   Usage: native-run ios [options]
 
+    Run an .app or .ipa on a device or simulator target
+
+    Targets are selected as follows:
+      1) --target using device/simulator UUID
+      2) A connected device, unless --simulator is used
+      3) A running simulator
+
+    If the above criteria are not met, the app is run on the default simulator
+    (the last simulator in the list).
+
+    Use --list to list available targets.
+
   Options:
 
+    --list ............... Print available targets, then quit
+    --json ............... Output JSON
+
+    --app <path> ......... Deploy specified .app or .ipa file
+    --device ............. Use a device if available
+                           With --list prints connected devices
+    --simulator .......... Use a simulator
+                           With --list prints available emulators
+    --emulator ........... Alias for --simulator
+    --target <id> ........ Use a specific target
+    --connect ............ Tie process to app process
 `;
 
 export async function run() {
-  process.stdout.write(help);
+  process.stdout.write(`${help}\n`);
 }
