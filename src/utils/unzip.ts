b77ad3a0ca2a60efae72a7ee4c3b17197b8b0a25
Tim Lancina <tim@ionic.io>
2019-04-25 15:07:21 -0700

    handle errors in unzip promise

--- src/utils/unzip.ts
+++ src/utils/unzip.ts
@@ -12,13 +12,17 @@ export async function unzip(srcPath: string, onEntry: UnzipOnEntry) {
   const open = promisify(yauzl.open.bind(yauzl) as YauzlOpen);
 
   return new Promise<void>(async (resolve, reject) => {
-    const zipfile = await open(srcPath, { lazyEntries: true });
-    const openReadStream = promisify(zipfile.openReadStream.bind(zipfile) as YauzlOpenReadStream);
-    zipfile.once('error', reject);
-    // resolve when either one happens
-    zipfile.once('close', resolve); // fd of zip closed
-    zipfile.once('end', resolve); // last entry read
-    zipfile.on('entry', entry => onEntry(entry, zipfile, openReadStream));
-    zipfile.readEntry();
+    try {
+      const zipfile = await open(srcPath, { lazyEntries: true });
+      const openReadStream = promisify(zipfile.openReadStream.bind(zipfile) as YauzlOpenReadStream);
+      zipfile.once('error', reject);
+      // resolve when either one happens
+      zipfile.once('close', resolve); // fd of zip closed
+      zipfile.once('end', resolve); // last entry read
+      zipfile.on('entry', entry => onEntry(entry, zipfile, openReadStream));
+      zipfile.readEntry();
+    } catch (error) {
+      reject(error);
+    }
   });
 }
