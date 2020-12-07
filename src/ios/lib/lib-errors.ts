b0ee2d4fc2c3edd57e7107aa1145ff1f1a96d002
Daniel Imhoff <dwieeb@gmail.com>
2020-12-07 10:39:33 -0800

    comment

--- src/ios/lib/lib-errors.ts
+++ src/ios/lib/lib-errors.ts
@@ -1,3 +1,6 @@
+/**
+ * Type union of error codes we get back from the protocol.
+ */
 export type IOSLibErrorCode = 'DeviceLocked';
 
 export class IOSLibError extends Error implements NodeJS.ErrnoException {
