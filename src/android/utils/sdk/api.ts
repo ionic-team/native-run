bc482319c77c79c0da77b769865ab070c68efb67
Mike Summerfeldt <20338451+IT-MikeS@users.noreply.github.com>
2022-05-12 11:54:05 -0400

    feat(android): add API 32 support (#232)

--- src/android/utils/sdk/api.ts
+++ src/android/utils/sdk/api.ts
@@ -7,6 +7,7 @@ import type * as Pixel_2_API_28 from '../../data/avds/Pixel_2_API_28.json';
 import type * as Pixel_3_API_29 from '../../data/avds/Pixel_3_API_29.json';
 import type * as Pixel_3_API_30 from '../../data/avds/Pixel_3_API_30.json';
 import type * as Pixel_3_API_31 from '../../data/avds/Pixel_3_API_31.json';
+import type * as Pixel_3_API_32 from '../../data/avds/Pixel_3_API_32.json';
 import type * as Pixel_API_25 from '../../data/avds/Pixel_API_25.json';
 
 import type { SDKPackage } from './';
@@ -86,6 +87,7 @@ export function findPackageBySchemaPath(
 }
 
 export type PartialAVDSchematic =
+  | typeof Pixel_3_API_32
   | typeof Pixel_3_API_31
   | typeof Pixel_3_API_30
   | typeof Pixel_3_API_29
@@ -106,6 +108,34 @@ export interface APISchema {
   readonly validate: (packages: readonly SDKPackage[]) => APISchemaPackage[];
   readonly loadPartialAVDSchematic: () => Promise<PartialAVDSchematic>;
 }
+
+export const API_LEVEL_32: APISchema = Object.freeze({
+  apiLevel: '32',
+  validate: (packages: readonly SDKPackage[]) => {
+    const schemas: APISchemaPackage[] = [
+      { name: 'Android Emulator', path: 'emulator', version: /.+/ },
+      {
+        name: 'Android SDK Platform 32',
+        path: 'platforms;android-32',
+        version: /.+/,
+      },
+    ];
+
+    const missingPackages = findUnsatisfiedPackages(packages, schemas);
+
+    if (!findPackageBySchemaPath(packages, /^system-images;android-32;/)) {
+      missingPackages.push({
+        name: 'Google Play Intel x86 Atom System Image',
+        path: 'system-images;android-32;google_apis_playstore;x86',
+        version: '/.+/',
+      });
+    }
+
+    return missingPackages;
+  },
+  loadPartialAVDSchematic: async () =>
+    import('../../data/avds/Pixel_3_API_32.json'),
+});
 
 export const API_LEVEL_31: APISchema = Object.freeze({
   apiLevel: '31',
@@ -332,6 +362,7 @@ export const API_LEVEL_24: APISchema = Object.freeze({
 });
 
 export const API_LEVEL_SCHEMAS: readonly APISchema[] = [
+  API_LEVEL_32,
   API_LEVEL_31,
   API_LEVEL_30,
   API_LEVEL_29,
