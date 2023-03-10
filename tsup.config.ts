3fbbdc211082519b1ab102655e0391e0fdd83e97
productdevbook <hi@productdevbook.com>
2023-03-10 15:30:31 +0300

    fix: ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import type { Options } from 'tsup'
 
 import pkg from './package.json'
 const external = [
-  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {}),
 ]
 
 export default <Options>{
